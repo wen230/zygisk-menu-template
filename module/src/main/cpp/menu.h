6108c8be17a868ee13fed1e96cdfcbc4a6c1e097
Karma <106445107+BoredKarma@users.noreply.github.com>
2023-04-19 06:56:03 -0700

    Initial commit

---
+++ module/src/main/cpp/menu.h
@@ -0,0 +1,77 @@
+//
+// Created by lbert on 4/15/2023.
+//
+
+#ifndef ZYGISK_MENU_TEMPLATE_MENU_H
+#define ZYGISK_MENU_TEMPLATE_MENU_H
+
+using namespace ImGui;
+
+void DrawMenu()
+{
+    static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    {
+        Begin(OBFUSCATE("ZyCheats"));
+        ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags_FittingPolicyResizeDown;
+        if (BeginTabBar("Menu", tab_bar_flags)) {
+            if (BeginTabItem(OBFUSCATE("Account"))) {
+                // here menu stuff, remove test btw
+                // ImGui::Checkbox(OBFUSCATE("This is a checkbox"), &test);
+                if (Button(OBFUSCATE("Add Currency"))) {
+                    // code for button action
+                    addCurrency = true;
+                }
+                TextUnformatted(OBFUSCATE("Adds 1000 gems"));
+                if (Button(OBFUSCATE("Add Skins"))) {
+                    // code for button action
+                    addSkins = true;
+                }
+                Checkbox(OBFUSCATE("Everything unlocked"), &everythingUnlocked);
+                Checkbox(OBFUSCATE("Free Items"), &freeItems);
+                Checkbox(OBFUSCATE("Show Items"), &showAllItems);
+                EndTabItem();
+            }
+            EndTabBar();
+        }
+        Patches();
+        End();
+    }
+}
+
+void SetupImgui() {
+    IMGUI_CHECKVERSION();
+    CreateContext();
+    ImGuiIO &io = GetIO();
+    io.DisplaySize = ImVec2((float) glWidth, (float) glHeight);
+    ImGui_ImplOpenGL3_Init("#version 100");
+    StyleColorsDark();
+    GetStyle().ScaleAllSizes(7.0f);
+    io.Fonts->AddFontFromMemoryTTF(Roboto_Regular, 30, 30.0f);
+}
+
+
+EGLBoolean (*old_eglSwapBuffers)(EGLDisplay dpy, EGLSurface surface);
+EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
+    eglQuerySurface(dpy, surface, EGL_WIDTH, &glWidth);
+    eglQuerySurface(dpy, surface, EGL_HEIGHT, &glHeight);
+
+    if (!setupimg)
+    {
+        SetupImgui();
+        setupimg = true;
+    }
+
+    ImGuiIO &io = GetIO();
+    ImGui_ImplOpenGL3_NewFrame();
+    NewFrame();
+
+    DrawMenu();
+
+    EndFrame();
+    Render();
+    glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    return old_eglSwapBuffers(dpy, surface);
+}
+
+#endif //ZYGISK_MENU_TEMPLATE_MENU_H
